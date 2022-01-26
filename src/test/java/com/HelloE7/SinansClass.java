d77f8a8fc63857ee26f3df762eead38bb5df3c50
Sinan Yilmaz <cakimentoglu@gmail.com>
2022-01-26 20:22:17 +0000

    my first commit

---
+++ src/test/java/com/HelloE7/SinansClass.java
@@ -0,0 +1,8 @@
+package com.HelloE7;
+
+public class SinansClass {
+
+    public static void main(String[] args) {
+        System.err.println("All of these codes are wrong, unfortunately");
+    }
+}
